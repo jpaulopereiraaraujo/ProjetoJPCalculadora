c21350960115222b0382f19b09a6e0773f9c1c1d
jpaulopereiraaraujo <jpaulopereiraaraujo@gmail.com>
2022-04-28 23:09:02 -0300

    Tratamento de ponto, comentários e adição de botões.

--- scripts.js
+++ scripts.js
@@ -24,22 +24,45 @@ function calcResultado(){
      
     i = 0;
     stringArray ='';
+    
+    //Esse laço realiza o tratamento da string, verificando se os números e operadores
+    //estão ok e pre parando-a para se tornar um array [numero, operador, numero]
     while (valueArray.length > i)
     {
         console.log(i);
-        if (isNaN(valueArray[i]) == false || '+-÷/*x'.includes(valueArray[i]) == true)
+        if (isNaN(valueArray[i]) == false || '+-÷/*x.'.includes(valueArray[i]) == true)
         {
-            
+            //Atenção o ponto primeiro é tratado como operador apenas para ser validado
+            //logo após o ponto é tratado como número para que possa ser feita operações
+            //decimais.
+
              if (isNaN(valueArray[0]) == true ){
                 alert('Atenção, a operação começa com um número e não com um operador')
                 calcLimpar();
-                i=valueArray.lengh + 1;    
+                break;    
             } else {
-                if ('0123456789'.includes(valueArray[i]) == true){
-                    stringArray = stringArray + valueArray[i];
+                if ('0123456789.'.includes(valueArray[i]) == true){
+                    //Verifica se o usuário digitou mais de um ponto.
+                    if (valueArray[i+1] == '.' || valueArray[i-1] == '.'){
+                        alert('Atenção, utilize apenas um ponto por número.')
+                        calcLimpar();
+                        break;
+                    } else {
+                        stringArray = stringArray + valueArray[i];
+                    }
+                    
                 }
                 if ('+-÷/*x'.includes(valueArray[i]) == true ) {
-                    stringArray = stringArray  + ',' + valueArray[i] + ',';
+                    
+                    //Tratamento para dois ou mais operadores
+                    if (valueArray[i] == valueArray[i-1] || valueArray[i] == valueArray[i+1] ){
+                        alert('Atenção, a operação permite apenas 1 operador em sequencia.')
+                        calcLimpar();
+                        break;
+                    } else {
+                        stringArray = stringArray  + ',' + valueArray[i] + ',';
+                    }
+                    
                 }
             }
                     
@@ -48,17 +71,54 @@ function calcResultado(){
         {
             alert('Atenção use apenas os caracteres de operação e números!!!')
             calcLimpar();
-            i=valueArray.lengh + 1;
+            break;
+            
         }
         
         i++;
         
     }
+
+    //funções de operação
+    //O parseFloat converte string em float
+
+    function soma(a,b){
+        resSoma = parseFloat(a) + parseFloat(b)
+        return resSoma;
+    }
+
+    function subtrair(a,b){
+        resSubtrair = parseFloat(a) + parseFloat(b)
+        return resSubtrair;
+    }
+
+    function multiplicar(a,b){
+        resMultiplicar = parseFloat(a) * parseFloat(b)
+        return resMultiplicar;
+    }
+
+    function dividir(a,b){
+        resDividir = parseFloat(a) / parseFloat(b)
+        return resDividir;
+    }
+    
+    
+    //refinedArray é a string organizada como array separada por número e operação.
     refinedArray = stringArray.split(',')
     console.log(refinedArray);
     j = 0;
-    while ( j < refinedArray.lengh ) {
+    
+    //Esse if irá percorrer o array em busca de um operador para definir a operação
+    if (valueArray.lengh == (i + 1)) {
+
+        while ( j < refinedArray.lengh ) {
         
+        if ('+-÷/*x'.includes(refinedArray[j]) == true){
+            
+
+        }
+    }
+    
     }
 
 }
